93dd957bb0202630c504200b460793a9b4537560
AKIRA <akira2@kaibarahikarinoMacBook-Pro.local>
2018-04-03 11:30:06 +0900

    非同期通信実装準備　1

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,25 @@
 $(function(){
+  function buildHTML(message){
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                    ${ message.name }
+                    </div>
+                    <div class="upper-message__date">
+                    ${ message.date }
+                    </div>
+                  </div>
+                  <div class="lower-meesage">
+                    <p class="lower-message__content">
+                    ${ message.content}
+                    </p>
+                  </div>
+              </div>`
+    return html;
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    console.log(this)
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
@@ -12,5 +30,14 @@ $(function(){
       processData: false,
       contentType: false
     })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+    })
+    fail(function(){
+      alert('error');
+    })
   })
+
+
 })
