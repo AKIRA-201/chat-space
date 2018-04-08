1d96d505873dc11fceabea0bbcb84a61a00ae807
AKIRA <akira2@kaibarahikarinoMacBook-Pro.local>
2018-04-08 10:17:10 +0900

    インクリメンタルサーチ 実装のステップ

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,33 +1,48 @@
-$(function(){
-  function buildHTML(message){
-    var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">ユーザー名</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</a>
-                </div>`
-    return html;
+$(function() {
+  var search_list = $('#user-search-result');
+
+  function appendUser(user) {
+    var html =
+    `<div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">${user.name}</p>
+      <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+    </div>`
+    search_list.append(html);
   }
 
-  $(function() {
-    $('.chat-group-form__input').on('keyup', function(){
-      var input = $(this).val();
-      console.log(input);
+  function appendNoUser(user){
+    var html =
+    `<div class='chat-group-user clearfix>
+      <p class='chat-group-user__name'>${user}</p>
+    </div>`
+    search_list.append(html);
+  }
+
+    $('#user-search-field').on('keyup',function() {
+      // console.log('this')
+      var input = $('#user-search-field').val();
+      if (input.length !== 0) {
       $.ajax({
         type: 'GET',
-        url: '/users/search',
+        url: '/users',
         data: { keyword: input },
         dataType:'json'
       })
       .done(function(users){
-        $('.chat-group-form__input').empty
-        if (users.length!== 0) {
+        // console.log(users);
+        $('#user-search-result').empty();
+        if (users.length !== 0) {
           users.forEach(function(user){
             appendUser(user);
           });
         }
-        .fail(function(){
-          appendNOuser('一致するユーザーはいません');
-        })
+        else{
+        appendNoUser('一致するユーザーはいません');
+        }
       })
-    });
+      .fail(function() {
+        alert('ユーザー検索に失敗しました')
+      })
+    }
   });
 });
--- app/views/users/search.json.builder
+++
@@ -1,4 +0,0 @@
-json.array! @users do |use|
-  json.id @use.id
-  json.group_id @group_id
-end
