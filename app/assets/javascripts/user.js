09b0a06aea8ae1f9476ce0cae2685b74ab13941d
AKIRA <akira2@kaibarahikarinoMacBook-Pro.local>
2018-04-08 17:16:36 +0900

    インクリメンタルサーチ後の実装のステップ
    インデックス画面がサンプルのままだったので修正
    検索結果からHTMLを消し、チャットメンバーの部分にHTMLを追加する
    削除を押すと、チャットメンバーから削除する
    ところまで

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -19,7 +19,6 @@ $(function() {
   }
 
     $('#user-search-field').on('keyup',function() {
-      // console.log('this')
       var input = $('#user-search-field').val();
       if (input.length !== 0) {
       $.ajax({
@@ -29,7 +28,6 @@ $(function() {
         dataType:'json'
       })
       .done(function(users){
-        // console.log(users);
         $('#user-search-result').empty();
         if (users.length !== 0) {
           users.forEach(function(user){
@@ -44,5 +42,25 @@ $(function() {
         alert('ユーザー検索に失敗しました')
       })
     }
+  });
+
+  function addUser(user_id,user_name){
+    var html =
+    `<div class='chat-group-user learfix js-chat-member' id='chat-group-user-8'>
+      <input name='group[user_ids][]' type='hidden' value='${user_id}'>
+        <p class='chat-group-user__name'>${user_name}</p>
+        <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+    </div>`
+    $('#chat-group-users').append(html);
+  }
+
+  $('#user-search-result').on('click', '.chat-group-user__btn--add', function() {
+    var user_id = $(this).data('user-id');
+    var user_name = $(this).data('user-name');
+    addUser(user_id,user_name);
+    $(this).parent().remove();
+  });
+    $('#chat-group-users').on('click','.chat-group-user__btn--remove', function() {
+      $(this).parent().remove();
   });
 });
