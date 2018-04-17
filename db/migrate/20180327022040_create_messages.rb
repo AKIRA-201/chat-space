3f970239b43eb2963ceca68fda4982ed0f6ecd8a
AKIRA <>
2018-04-17 16:05:07 +0900

    fix

--- db/migrate/20180327022040_create_messages.rb
+++ db/migrate/20180327022040_create_messages.rb
@@ -1,8 +1,8 @@
 class CreateMessages < ActiveRecord::Migration[5.0]
   def change
     create_table :messages do |t|
-      t.string  :content
-      t.string  :image
+      t.text  :content
+      t.text  :image
       t.references :group, foreign_key: true, index: true
       t.references :user, foreing_key: true, index: true
       t.timestamps
