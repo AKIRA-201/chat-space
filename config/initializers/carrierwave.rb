201b072f5168fda133f9b23904aaa1abe26353fa
AKIRA <>
2018-04-17 16:14:17 +0900

    fix blood

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,9 +8,9 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'us-east-1'
+    region: 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'akira3'
-  config.asset_host = 'https://s3-eu-west-3.amazonaws.com/akira3'
+  config.fog_directory  = 'akira6'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/akira6'
 end
